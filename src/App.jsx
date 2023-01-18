788de9f40bd7d9fc5fb66312e3dcee74f0fa396d
Kostadin Mihaylov <kmihaylov@90gmail.com>
2023-01-18 16:26:01 +0200

    Initial Clean up the project and add some assets.

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "../src/app.css"
+
+function App() {
+  return (
+    <div className="app">
+     <div className="main">main</div>
+     <div className="pyramid">money</div>
+     <ul className="moneyList">
+      <li className="moneyListItem">100 000 лв</li>
+      <li className="moneyListItem">50 000 лв</li>
+      <li className="moneyListItem">20 000 лв</li>
+      <li className="moneyListItem">10 000 лв</li>
+      <li className="moneyListItem">5 000 лв</li>
+      <li className="moneyListItem">2 000 лв</li>
+      <li className="moneyListItem">1 000 лв</li>
+      <li className="moneyListItem">500 лв</li>
+      <li className="moneyListItem">400 лв</li>
+      <li className="moneyListItem">300 лв</li>
+      <li className="moneyListItem">200 лв</li>
+      <li className="moneyListItem">100 лв</li>
+     </ul>
+    </div>
+  );
+}
+
+export default App;
