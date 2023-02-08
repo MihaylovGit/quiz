c5182346eb275bde8ba054217cf1e29391bdb8c2
Kostadin Mihaylov <kmihaylov@90gmail.com>
2023-02-08 13:39:35 +0200

    Create Timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,154 @@
-import { Component, useState } from "react";
+import { Component, useMemo, useState } from "react";
 import "../src/app.css";
-import Quiz from "./components/quiz";
+import Quiz from "./components/Quiz.jsx";
 
 function App() {
-  const [questionNumber, setQuestionNumber] = useState(1); 
-  const moneyPyramid = [
-   {id: 1, amount:"100 лв"},
-   {id: 2, amount:"200 лв"},
-   {id: 3, amount:"300 лв"},
-   {id: 4, amount:"400 лв"},
-   {id: 5, amount:"500 лв"},
-   {id: 6, amount:"1 000 лв"},
-   {id: 7, amount:"1 500 лв"},
-   {id: 8, amount:"2 000 лв"},
-   {id: 9, amount:"3 000 лв"},
-   {id: 10, amount:"5 000 лв"},
-   {id: 11, amount:"10 000 лв"},
-   {id: 12, amount:"20 000 лв"},
-   {id: 13, amount:"30 000 лв"},
-   {id: 14, amount:"50 000 лв"},
-   {id: 15, amount:"100 000 лв"},
-  ].reverse();
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [stop, setStop] = useState(false);
+  const[earned, setEarned] = useState("$ 0");
+
+  const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "When did the website `Facebook` launch?",
+      answers: [
+        {
+          text: "2004",
+          correct: true,
+        },
+        {
+          text: "2005",
+          correct: false,
+        },
+        {
+          text: "2006",
+          correct: false,
+        },
+        {
+          text: "2007",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+  ];
+
+  const moneyPyramid = useMemo(()=>
+    [
+      { id: 1, amount: "100 лв" },
+      { id: 2, amount: "200 лв" },
+      { id: 3, amount: "300 лв" },
+      { id: 4, amount: "400 лв" },
+      { id: 5, amount: "500 лв" },
+      { id: 6, amount: "1 000 лв" },
+      { id: 7, amount: "1 500 лв" },
+      { id: 8, amount: "2 000 лв" },
+      { id: 9, amount: "3 000 лв" },
+      { id: 10, amount: "5 000 лв" },
+      { id: 11, amount: "10 000 лв" },
+      { id: 12, amount: "20 000 лв" },
+      { id: 13, amount: "30 000 лв" },
+      { id: 14, amount: "50 000 лв" },
+      { id: 15, amount: "100 000 лв" },
+    ].reverse(),
+  [] ); 
+
+  useEffect(() => {
+    questionNumber > 1 &&
+      setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+  }, [questionNumber, moneyPyramid]);
+
   return (
     <div className="app">
-     <div className="main">main</div>
-     <div className="top"></div>
-     <div className="timer">30</div>
-     <div className="bottom">
-    <Quiz/>
-     </div>
-     <div className="pyramid">money</div>
-     <ul className="moneyList">
-      {moneyPyramid.map((m) => (
-        <li className={questionNumber === m.id ? "moneyListItem active" : "moneyListItem"}>
-      <span className="moneyListItemNumber">{m.id}</span>
-      <span className="moneyListItemAmount">{m.amount}</span>
-      </li>
-      ))}
-     </ul>
+      {!username ? (
+        <Start setUsername={setUsername} />
+      ) : (
+        <>
+          <div className="main">
+            {timeOut ? (
+              <h1 className="endText">You earned: {earned}</h1>
+            ) : (
+              <>
+                <div className="top">
+                  <div className="timer">
+                    <Timer
+                      setTimeOut={setTimeOut}
+                      questionNumber={questionNumber}
+                    />
+                  </div>
+                </div>
+                <div className="bottom">
+                  <Quiz
+                    data={data}
+                    questionNumber={questionNumber}
+                    setQuestionNumber={setQuestionNumber}
+                    setTimeOut={setTimeOut}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          <div className="pyramid">
+            <ul className="moneyList">
+              {moneyPyramid.map((m) => (
+                <li
+                  className={
+                    questionNumber === m.id
+                      ? "moneyListItem active"
+                      : "moneyListItem"
+                  }
+                >
+                  <span className="moneyListItemNumber">{m.id}</span>
+                  <span className="moneyListItemAmount">{m.amount}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -1,13 +1,47 @@
-export default function Quiz() {
+import { useEffect, useState } from "react"
+
+export default function Quiz({
+    data,
+    setStop,
+    questionNumber,
+    setQuestionNumber }) {
+    const [question, setQuestion] = useState(null);
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [className, setClassName] = useState("answer");
+
+    useEffect(() => {
+        setQuestion(data[questionNumber - 1])
+    }, [data, questionNumber]);
+
+    const delay = (duration, callback) => {
+        setTimeout(() => {
+            callback();
+        }, duration);
+
+        delay(6000, (a) => {
+            if (a.correct) {
+                setQuestionNumber = ((prev) => prev + 1);
+                setSelectedAnswer(null);
+            } else{
+                setStop(true);
+            }
+        });
+    };
+
+    const handleClick = () => {
+        setSelectedAnswer();
+        setClassName("answer active");
+        delay(3000, (a) => setClassName(a.correct ? "answer correct" : "answer wrong"));
+    };
+
     return (
         <div className="quiz">
-<div className="question">Коя е столицата на Испания?</div>
-<div className="answers">
-    <div className="answer">Мадрид</div>
-    <div className="answer">Лисабон</div>
-    <div className="answer">Техеран</div>
-    <div className="answer">Валенсия</div>
-</div>
+            <div className="question">{question?.question}</div>
+            <div className="answers">
+                {question?.answers.map((a) => (
+                    <div className={selectedAnswer === a ? className : "answer"} onClick={() => handleClick(a)}>{a.text}</div>
+                ))}
+            </div>
         </div>
     )
 }
